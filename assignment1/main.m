c7070b9f5606eea65f28b1aa986204498d7aefbf
Chua Jie Sheng <chuajiesheng@gmail.com>
2016-08-27 18:47:59 +0800

    Made main.m a function that is runnable from command window

--- assignment1/main.m
+++ assignment1/main.m
@@ -11,55 +11,56 @@
 %The function "data_split" is provided as an example. You can rewrite it.
 %
 %
+function main(file, model_name)
+    fprintf('Starting\n');
 
-%Command line parameters
-arg_list = argv ();
+    %Loading data
+    file_path = strcat('../data/', file);
+    fprintf('Using file: %s\n', file_path);
+    data = importdata(file_path);
+    
+    %Identify the model name
+    fprintf('Using model: %s\n', model_name);
 
-%Loading data
-file_path = strcat("../data/", arg_list{1});
-data = importdata(file_path);
+    %Split the data into training, validation and test data sets, X is feature, Y is label
+    %Some data may need normalization on the features
+    [X_train, Y_train, X_val, Y_val, X_test, Y_test] = data_split(data);
 
-%Identify the model name
-model_name = arg_list{2};
-
-%Split the data into training, validation and test data sets, X is feature, Y is label
-%Some data may need normalization on the features
-[X_train, Y_train, X_val, Y_val, X_test, Y_test] = data_split(data);
-
-%Weight initialization
-feature_size = size(X_train, 2);
-weights = randn(feature_size, 1) * 0.5;
-bias = randn(1);
+    %Weight initialization
+    feature_size = size(X_train, 2);
+    weights = randn(feature_size, 1) * 0.5;
+    bias = randn(1);
 
 
-if strcmp(model_name, "analytical")
-	%Training
-	[loss_train, weights, bias] = analytical_train(X_train, Y_train, weights, bias);
-	%Evauate on validation data set
-	loss_val   = analytical_test(X_val, Y_val, weights, bias);
-	%Evaluate on testing data set
-	loss_test  = analytical_test(X_test, Y_test, weights, bias);
-	
+    if strcmp(model_name, 'analytical')
+        %Training
+        [loss_train, weights, bias] = analytical_train(X_train, Y_train, weights, bias);
+        %Evauate on validation data set
+        loss_val   = analytical_test(X_val, Y_val, weights, bias);
+        %Evaluate on testing data set
+        loss_test  = analytical_test(X_test, Y_test, weights, bias);
 
-elseif strcmp(model_name, "iterative")
-	iterations = 1000;
-	for i = 1:1:iterations
-		%Training		 
-		[loss_train, weights, bias] = linearR_train(X_train, Y_train, weights, bias);
-		if i <= 10
-		    printf("Iteration %d loss: %f\n", i, loss_train);
-		end
-		%Evauate on validation data set
-		loss_val = linearR_predict(X_val, Y_val, weights, bias);
-	end
-	%Evaluate on testing data set
-	loss_test = linearR_predict(X_test, Y_test, weights, bias);
-else
-	printf("Training model should be provided !!!!! \n")
-	return
+
+    elseif strcmp(model_name, 'iterative')
+        iterations = 1000;
+        for i = 1:1:iterations
+            %Training		 
+            [loss_train, weights, bias] = linearR_train(X_train, Y_train, weights, bias);
+            if i <= 10
+                printf('Iteration %d loss: %f\n', i, loss_train);
+            end
+            %Evauate on validation data set
+            loss_val = linearR_predict(X_val, Y_val, weights, bias);
+        end
+        %Evaluate on testing data set
+        loss_test = linearR_predict(X_test, Y_test, weights, bias);
+    else
+        printf('Training model should be provided !!!!! \n')
+        return
+    end
+
+
+    fprintf('Final loss for training data: %f\n', loss_train);
+    fprintf('Final loss for validation data: %f\n', loss_val);
+    fprintf('Final loss for test data: %f\n', loss_test);
 end
-
-
-printf("Final loss for training data: %f\n", loss_train);
-printf("Final loss for validation data: %f\n", loss_val);
-printf("Final loss for test data: %f\n", loss_test);
