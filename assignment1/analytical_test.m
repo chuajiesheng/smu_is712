661fb22467b4d64f9f32d2e4a1684d0c383f52fe
Chua Jie Sheng <chuajiesheng@gmail.com>
2016-08-27 21:38:14 +0800

    Loss function should be (wx-y)^2 / 2

--- assignment1/analytical_test.m
+++ assignment1/analytical_test.m
@@ -1,6 +1,6 @@
 function loss = analytical_test(X, Y, weights, bias)
 
     predict = (weights.' * X.').' + bias
-    loss = sum((Y - predict).^2)
+    loss = mean2((predict - Y).^2 / 2)
     
 end
--- assignment1/analytical_train.m
+++ assignment1/analytical_train.m
@@ -2,9 +2,7 @@ function [loss, weights, bias] = analytical_train(X, Y, weights, bias)
 	
     % weight = [ (x^T * x)^-1 * x^T] * y
  	weights = (X.' * X) \ X.' * Y; 
-	bias    = bias;
-    
     predict = (weights.' * X.').' + bias
-    loss = sum((Y - predict).^2)
+    loss = mean2((predict - Y).^2 / 2)
 
 end
