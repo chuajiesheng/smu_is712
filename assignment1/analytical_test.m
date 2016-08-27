652c66c28d1f9cd72c2f26cfc35e0a773796969b
Chua Jie Sheng <chuajiesheng@gmail.com>
2016-08-27 18:48:48 +0800

    Implemented normal equation method

--- assignment1/analytical_test.m
+++ assignment1/analytical_test.m
@@ -1,5 +1,7 @@
 function loss = analytical_test(X, Y, weights, bias)
 
-	loss = ;
+    weights = ((X.' * X) \ X.') * Y; 
+    predict = (X * weights) - Y + bias
+	loss    = sum((predict - Y).^2)
 
 end
--- assignment1/analytical_train.m
+++ assignment1/analytical_train.m
@@ -1,7 +1,9 @@
 function [loss, weights, bias] = analytical_train(X, Y, weights, bias)
 	
-	weights = ;
-	bias    = ;
-	loss    = ; 
+    % weight = [ (x^T * x)^-1 * x^T] * y
+ 	weights = ((X.' * X) \ X.') * Y; 
+	bias    = bias;
+    predict = (X * weights) - Y + bias
+	loss    = sum((predict - Y).^2)
 
 end
