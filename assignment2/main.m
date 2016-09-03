2a295128c37059f354dad98385a866c7ba965894
Chua Jie Sheng <chuajiesheng@gmail.com>
2016-09-03 15:44:39 +0800

    Convert octave function to matlab

--- assignment2/main.m
+++ assignment2/main.m
@@ -11,55 +11,50 @@
 %The function "data_split" is provided as an example. You can rewrite it.
 %
 %
-
-%Command line parameters
-arg_list = argv ();
-
-%Loading data
-data_name = arg_list{1};
+function main(data_name, model_name) 
+    %data_name: Loading data - previously arg_list{1}
+    %model_name: Identify the model name - previously arg_list{2}
 
 
-%Identify the model name
-model_name = arg_list{2};
+    %Split the data into training, validation and test data sets, X is feature, Y is label
+    %Some data may need normalization on the features
+    [X_train, Y_train, X_val, Y_val, X_test, Y_test] = data_split(data_name);
 
-%Split the data into training, validation and test data sets, X is feature, Y is label
-%Some data may need normalization on the features
-[X_train, Y_train, X_val, Y_val, X_test, Y_test] = data_split(data_name);
-
-%Weight initialization
-feature_size = size(X_train, 2);         %The first feature equals 1 for all instances.
-weights = randn(feature_size, 1) * 0.05; %Bias is integrated in weights. 
-%bias = randn(1);
+    %Weight initialization
+    feature_size = size(X_train, 2);         %The first feature equals 1 for all instances.
+    weights = randn(feature_size, 1) * 0.05; %Bias is integrated in weights. 
+    %bias = randn(1);
 
 
-if strcmp(model_name, "fminunc")
-	%Training
-	[loss_train, weights] = fminunc_train(X_train, Y_train, weights);
-	%Evauate on validation data set
-	accuracy_val = logisticR_predict(X_val, Y_val, weights);
-	%Evaluate on testing data set
-	accuracy_test = logisticR_predict(X_test, Y_test, weights);
-	
+    if strcmp(model_name, 'fminunc')
+        %Training
+        [loss_train, weights] = fminunc_train(X_train, Y_train, weights);
+        %Evauate on validation data set
+        accuracy_val = logisticR_predict(X_val, Y_val, weights);
+        %Evaluate on testing data set
+        accuracy_test = logisticR_predict(X_test, Y_test, weights);
 
-elseif strcmp(model_name, "batch")
-	iterations = 100;
-	for i = 1:1:iterations
-		%Training		 
-		[loss_train, weights] = logisticR_train(X_train, Y_train, weights);		
-		if i <= 10
-		    printf("Iteration %d loss: %f\n", i, loss_train);
-		end
-		%Evauate on validation data set
-		accuracy_val = logisticR_predict(X_val, Y_val, weights);
-	end
-	%Evaluate on testing data set
-	accuracy_test = logisticR_predict(X_test, Y_test, weights);
-else
-	printf("Training model should be provided !!!!! \n")
-	return
+
+    elseif strcmp(model_name, 'batch')
+        iterations = 100;
+        for i = 1:1:iterations
+            %Training		 
+            [loss_train, weights] = logisticR_train(X_train, Y_train, weights);		
+            if i <= 10
+                printf('Iteration %d loss: %f\n', i, loss_train);
+            end
+            %Evauate on validation data set
+            accuracy_val = logisticR_predict(X_val, Y_val, weights);
+        end
+        %Evaluate on testing data set
+        accuracy_test = logisticR_predict(X_test, Y_test, weights);
+    else
+        printf('Training model should be provided !!!!! \n')
+        return
+    end
+
+
+    printf('Final loss for training data: %f\n', loss_train);
+    printf('Final accuracy for validation data: %f\n', accuracy_val);
+    printf('Final accuracy for test data: %f\n', accuracy_test);
 end
-
-
-printf("Final loss for training data: %f\n", loss_train);
-printf("Final accuracy for validation data: %f\n", accuracy_val);
-printf("Final accuracy for test data: %f\n", accuracy_test);
